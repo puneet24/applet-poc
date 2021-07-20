d380f21617cc6a2265ac7f7d8e28a13396a023a6
Puneet Agarwal <puneet.241994.agarwal@gmail.com>
2021-07-20 15:26:32 +0530

    done

--- src/Applet.js
+++ src/Applet.js
@@ -125,7 +125,8 @@ function Applet({ id, cacheManager }) {
                         if(!window.collab) return;
                         let strVal = api.getValueString(objName);
                         let parent = window.parent;
-                        for(let i=0;i<100;i++) {
+                        console.log(`batchsimulation :- `, window.batchSimulation);
+                        for(let i=0;i<window.batchSimulation;i++) {
                             parent.postMessage(JSON.stringify({
                                 id: idParam, msg: {
                                     listener: 'UPDATE_LISTENER',
